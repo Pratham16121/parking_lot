8bb13932fe5ce51753295342379fbbc93d499e00
Pratham Goel <pratham.goel16@gmail.com>
2023-06-09 23:47:54 +0530

    Added the API for the index of the car. Also the search feature

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,9 +4,18 @@ class CarsController < ApplicationController
     render json: result, status: result[:status]
   end
 
+  def index
+    result = Cars::Index.new(search_parmas).call
+    render json: result, status: result[:status]
+  end
+
   private
 
   def cars_params
     params.require(:car_data).permit(:registration, :color)
+  end
+
+  def search_parmas
+    params.permit(:registration, :color)
   end
 end
