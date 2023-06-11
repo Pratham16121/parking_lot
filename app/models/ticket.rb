2ebf7d479a631c6ab3bda3d3a689dee65cbb3c74
Pratham Goel <pratham.goel16@gmail.com>
2023-06-11 12:20:33 +0530

    Added a validation on ticket
    Validation says that there can be only one active ticekt for a particular car

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :slot, foreign_key: "assigned_slot"
   after_save :fill_slot, unless: :is_deleted?
   after_update :empty_slot, if: :is_deleted?
+  validate :validate_unique_active_ticket, on: :create
 
   private
 
@@ -12,4 +13,11 @@ class Ticket < ApplicationRecord
   def empty_slot
     self.slot.update!(is_empty: true, ticket_id: nil)
   end
+
+  def validate_unique_active_ticket
+    if Ticket.exists?(car_id: car_id, is_deleted: false)
+      errors.add(:base, 'Only one active ticket is allowed per car')
+    end
+  end
+
 end
